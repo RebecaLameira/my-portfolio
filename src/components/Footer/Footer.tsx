7c5b25e9e0e6cbc499668b1194a0660fe39b527a
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2023-11-30 17:29:14 -0400

    Fix footer.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,30 +1,27 @@
 import Link from "next/link";
 const Footer = () => {
 	return (
-		<footer className="bg-white rounded-lg shadow m-4">
+		<footer className="bg-white shadow">
 			<div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
 				<span className="font-indie text-2xl">Rebeca Lameira</span>
-				<ul className="flex flex-wrap justify-between items-center mt-3 text-sm ">
+				<ul className="flex flex-wrap gap-8 justify-between items-center mt-3 text-sm ">
 					<li>
-						<Link
-							href="/Contact"
-							className="hover:underline me-4 md:me-6 cursor-pointer"
-						>
+						<Link href="/Contact" className=" me-4 md:me-6 cursor-pointer">
 							<img src="/img/github.svg" />
 						</Link>
 					</li>
 					<li>
-						<Link href="/Contact" className="hover:underline me-4 md:me-6">
+						<Link href="/Contact" className=" me-4 md:me-6">
 							{<img src="/img/Linkedin.svg"></img>}
 						</Link>
 					</li>
 					<li>
-						<Link href="/Contact" className="hover:underline me-4 md:me-6">
+						<Link href="/Contact" className=" me-4 md:me-6">
 							{<img src="/img/Figma.svg"></img>}
 						</Link>
 					</li>
 					<li>
-						<Link href="/Contact" className="hover:underline me-4 md:me-6">
+						<Link href="/Contact" className=" me-4 md:me-6">
 							{<img src="/img/Discord.svg"></img>}
 						</Link>
 					</li>
