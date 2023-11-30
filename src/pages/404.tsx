d856a540412ecaf8fbf2a404eaa04185b5e63b88
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2023-11-30 17:27:30 -0400

    Customizing the 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,23 @@
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/NavBar/Navbar";
+import Head from "next/head";
+import Image from "next/image";
 export default function Custom404() {
-	return <h1>404 - Page Not Found</h1>;
+	return (
+		<>
+			<Head>
+				<title>Portfolio Rebeca Lameira</title>
+			</Head>
+			<Navbar path="/">{"Voltar"}</Navbar>
+			<div className="flex items-end">
+				<Image
+					src="/img/404.svg"
+					alt="Picture of the author"
+					width={800}
+					height={415}
+				/>
+			</div>
+			<Footer />
+		</>
+	);
 }
