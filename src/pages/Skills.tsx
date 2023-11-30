7f002f343938b5f1e05f4b551bab1aa002e1c81d
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2023-11-30 17:57:40 -0400

    Fix skills

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/NavBar/Navbar";
 import Head from "next/head";
 export default function Skills() {
 	return (
@@ -5,7 +7,9 @@ export default function Skills() {
 			<Head>
 				<title>Skills - Portfolio Rebeca Lameira</title>
 			</Head>
+			<Navbar path="/Contact">{"Contact me"}</Navbar>
 			<h1>Skills</h1>
+			<Footer />
 		</div>
 	);
 }
