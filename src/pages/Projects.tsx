64b3b03a461008a5a8c8b2066e1d98be0bf813a0
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2023-11-30 17:54:05 -0400

    Fix projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/NavBar/Navbar";
+import Footer from "@/components/Footer/Footer";
 import Head from "next/head";
 export default function Projects() {
 	return (
@@ -5,7 +7,9 @@ export default function Projects() {
 			<Head>
 				<title>Projects - Portfolio Rebeca Lameira</title>
 			</Head>
+			<Navbar path="/Contact">{"Contact me"}</Navbar>
 			<h1>Projects</h1>
+			<Footer />
 		</div>
 	);
 }
