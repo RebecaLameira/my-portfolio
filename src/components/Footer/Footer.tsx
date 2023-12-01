6a9c1c13b0c8ffbba6749343eb5532b9d58d6c96
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2023-11-30 21:06:52 -0400

    Try footer.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 const Footer = () => {
 	return (
 		<footer className="bg-white border-t-2">
-			<div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+			<div className=" flex items-stretch w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between self-end">
 				<span className="font-indie text-2xl">Rebeca Lameira</span>
 				<ul className="flex flex-wrap gap-8 justify-between items-center mt-3 text-sm ">
 					<li>
