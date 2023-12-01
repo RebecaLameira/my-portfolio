55157d9a7570a90c45f8900e97779f407655a3b2
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2023-12-01 12:31:43 -0400

    Add the favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<link rel="icon" href="/img/RL.ico" sizes="any" />
+			</Head>
 			<body className="h-screen flex bg-fundo">
 				<Main />
 				<NextScript />
