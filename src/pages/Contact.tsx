c52209ad9e38aaf29ae439020f4089c74a03a048
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2023-12-05 00:25:04 -0400

    First design implementation.

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,8 @@
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/NavBar/Navbar";
+import Input from "@/components/Input/Input";
 import Head from "next/head";
+import Button from "@/components/Button/Button";
 
 export default function Contact() {
 	return (
@@ -8,8 +10,24 @@ export default function Contact() {
 			<Head>
 				<title>Contact- Portfolio Rebeca Lameira</title>
 			</Head>
-			<Navbar path="/"> {"Voltar"}</Navbar>
-			<h1>Contact</h1>
+			<Navbar path="/"> {"Go back"}</Navbar>
+			<section className="grid place-content-center">
+				<div className="grid box-content justify-center my-12 h-[600px] w-[800px] bg-gradient-to-r from-[#26212C] via-[#376549] to-[#0E864C]  rounded-[50px] text-white ">
+					<h1 className="mt-12 text-5xl font-bold ">Contact Me</h1>
+					<p className="text-xl">
+						Did you like the Portfolio? Or would you like to send me a
+						statement.
+					</p>
+					<div>
+						<form className="grid gap-2">
+							<Input />
+							<Input />
+							<Input />
+							<Button>{"send me"}</Button>
+						</form>
+					</div>
+				</div>
+			</section>
 			<Footer></Footer>
 		</div>
 	);
