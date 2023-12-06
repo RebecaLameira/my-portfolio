c21828dd7fab629b5fc6e6d6504de6f16e78d371
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2023-12-05 21:22:48 -0400

    Fixing the footer.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 const Footer = () => {
 	return (
-		<footer className="bg-white border-t-2">
-			<div className=" flex items-center w-full mx-auto max-w-screen-xl py-4 px-8 justify-between lg:justify-center md:justify-center sm:justify-center">
-				<span className="font-indie text-2xl lg:hidden md:hidden sm:hidden">
-					Rebeca Lameira
-				</span>
-				<ul className="flex gap-8 justify-between items-center mt-3 text-sm lg:gap-20 md:gap-12">
+		<footer className="flex w-full max-h-32 py-4 px-16 bg-white border-t-2 fixed md:relative sm:relative inset-x-0 bottom-0 items-center justify-between lg:justify-center md:justify-center sm:justify-center">
+			<span className="font-indie text-2xl lg:hidden md:hidden sm:hidden">
+				Rebeca Lameira
+			</span>
+			<div>
+				<ul className="flex justify-between mt-3 text-sm gap-20 ">
 					<li>
 						<Link href="/Contact" className=" me-4 md:me-6 ">
 							<img src="/img/github.svg" />
