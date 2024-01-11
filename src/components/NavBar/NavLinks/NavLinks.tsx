8fd875dfbaa2de1012da83f68618db80b50fa0d4
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2024-01-11 01:48:36 -0400

    Including hover.

--- src/components/NavBar/NavLinks/NavLinks.tsx
+++ src/components/NavBar/NavLinks/NavLinks.tsx
@@ -2,11 +2,31 @@ import Link from "next/link";
 
 const NavLinks = () => {
 	return (
-		<div className="flex gap-8 laptop:shrink mobile:text-lg">
-			<Link href="/">Início</Link>
-			<Link href="/AboutMe"> Sobre Mim</Link>
-			<Link href="/Projects">Projetos </Link>
-			<Link href="/Skills"> Habilidades</Link>
+		<div className="flex gap-8 laptop:shrink mobile:text-lg ">
+			<Link
+				href="/"
+				className="no-underline decoration-emerald-500 decoration-2 hover:underline underline-offset-4"
+			>
+				Início
+			</Link>
+			<Link
+				href="/AboutMe"
+				className="no-underline decoration-emerald-500 decoration-2 hover:underline underline-offset-4"
+			>
+				Sobre Mim
+			</Link>
+			<Link
+				href="/Projects"
+				className="no-underline decoration-emerald-500 decoration-2 hover:underline underline-offset-4"
+			>
+				Projetos
+			</Link>
+			<Link
+				href="/Skills"
+				className="no-underline decoration-emerald-500 decoration-2 hover:underline underline-offset-4"
+			>
+				Habilidades
+			</Link>
 		</div>
 	);
 };
