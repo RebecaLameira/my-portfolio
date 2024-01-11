f2eee7bec8e4739208c791dca8b4e3e376d2b7e3
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2024-01-11 03:52:19 -0400

    Fix about me page.

--- src/components/Trajectory/Trajectory.tsx
+++ src/components/Trajectory/Trajectory.tsx
@@ -1,110 +1,115 @@
+import Title from "../Title/Title";
 const Trajectory = () => {
 	return (
-		<div className="flex h-screen items-center justify-center bg-white px-16 laptop:px-60">
-			<div className="space-y-6 border-l-2 border-dashed">
-				<div className="relative w-full">
-					<svg
-						viewBox="0 0 24 24"
-						fill="currentColor"
-						className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-secondary"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-					<div className="ml-6">
-						<h4 className="font-bold text-secondary text-xl">
-							Desenvolvedora Freelancer
-						</h4>
-						<p className="mt-2 max-w-screen-sm text-base text-gray-700">
-							Trabalhando com a linguagem TypeScript, e a biblioteca React.
-							Utilizando containers do Docker e para a contemporização o
-							Material UI.
-						</p>
-						<span className="mt-1 block text-sm font-semibold text-gray-400">
-							2023
-						</span>
+		<div className="bg-white m-16 p-8 mobile:m-8">
+			<Title>Trajetoria</Title>
+			<div className="flex h-screen items-center justify-center bg-white px-16 mobile:px-4 mobile:relative">
+				<div className="space-y-6 border-l-2 border-dashed">
+					<div className="relative w-full">
+						<svg
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-secondary"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<div className="ml-6">
+							<h4 className="font-bold text-secondary text-xl mobile:text-base">
+								Desenvolvedora Freelancer
+							</h4>
+							<p className="mt-2 max-w-screen-sm text-base text-gray-700 mobile:text-sm">
+								Trabalhando com a linguagem TypeScript, e a biblioteca React.
+								Utilizando containers do Docker e para a contemporização o
+								Material UI.
+							</p>
+							<span className="mt-1 block text-sm font-semibold text-gray-400 mobile:text-sm">
+								2023
+							</span>
+						</div>
 					</div>
-				</div>
-				<div className="relative w-full">
-					<svg
-						viewBox="0 0 24 24"
-						fill="currentColor"
-						className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-secondary"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-					<div className="ml-6">
-						<h4 className="font-bold text-secondary text-xl">
-							Desenvolvedora Front-End | Eu quero Grana
-						</h4>
-						<p className="mt-2 max-w-screen-sm text-base text-gray-700">
-							Setor de Desenvolvimento. Trabalhando com as linguagens de
-							programação JavaScript, TypeScript. E React.js e Next.js .
-							Utilizando a plataforma Discord para reuniões diárias, seguindo o
-							framework de gerenciamento Scrum.
-						</p>
-						<span className="mt-1 block text-sm font-semibold text-gray-400">
-							2021 - 2022
-						</span>
+					<div className="relative w-full ">
+						<svg
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-secondary "
+						>
+							<path
+								fill-rule="evenodd"
+								d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<div className="ml-6">
+							<h4 className="font-bold text-secondary text-xl mobile:text-base">
+								Desenvolvedora Front-End | Eu quero Grana
+							</h4>
+							<p className="mt-2 max-w-screen-sm text-base text-gray-700 mobile:text-sm">
+								Setor de Desenvolvimento. Trabalhando com as linguagens de
+								programação JavaScript, TypeScript. E React.js e Next.js .
+								Utilizando a plataforma Discord para reuniões diárias, seguindo
+								o framework de gerenciamento Scrum.
+							</p>
+							<span className="mt-1 block text-sm font-semibold text-gray-400 mobile:text-sm">
+								2021 - 2022
+							</span>
+						</div>
 					</div>
-				</div>
-				<div className="relative w-full">
-					<svg
-						viewBox="0 0 24 24"
-						fill="currentColor"
-						className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-secondary"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-					<div className="ml-6">
-						<h4 className="font-bold text-secondary text-xl">
-							Desenvolvedora Full Stack Estagiário | NUTI
-						</h4>
-						<p className="mt-2 max-w-screen-sm text-base text-gray-700">
-							Setor de Desenvolvimento. Trabalhando com as linguagens de
-							programação C#, Javascript, jQuery, HTML e o framework .NET e
-							AJAX. Utilizando SQL para o banco de dados.
-						</p>
-						<span className="mt-1 block text-sm font-semibold text-gray-400">
-							2021
-						</span>
+					<div className="relative w-full ">
+						<svg
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-secondary"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<div className="ml-6">
+							<h4 className="font-bold text-secondary text-xl mobile:text-base">
+								Desenvolvedora Full Stack Estagiário | NUTI
+							</h4>
+							<p className="mt-2 max-w-screen-sm text-base text-gray-700 mobile:text-sm">
+								Setor de Desenvolvimento. Trabalhando com as linguagens de
+								programação C#, Javascript, jQuery, HTML e o framework .NET e
+								AJAX. Utilizando SQL para o banco de dados.
+							</p>
+							<span className="mt-1 block text-sm font-semibold text-gray-400 mobile:text-sm">
+								2021
+							</span>
+						</div>
 					</div>
-				</div>
-				<div className="relative w-full">
-					<svg
-						viewBox="0 0 24 24"
-						fill="currentColor"
-						className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-secondary"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-					<div className="ml-6">
-						<h4 className="font-bold text-secondary text-xl">
-							Estagiário | UFMT
-						</h4>
-						<p className="mt-2 max-w-screen-sm text-base text-gray-700">
-							Suporte técnico de computadores e Equipamentos. Help Desk, fornece
-							suporte técnico e assistência aos usuários da empresa. Diagnóstico
-							de Problemas, atendimento a chamados, gerenciamento de senhas.
-						</p>
-						<span className="mt-1 block text-sm font-semibold text-gray-400">
-							2019 - 2021
-						</span>
+					<div className="relative w-full">
+						<svg
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-secondary"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<div className="ml-6">
+							<h4 className="font-bold text-secondary text-xl mobile:text-base">
+								Estagiário | UFMT
+							</h4>
+							<p className="mt-2 max-w-screen-sm text-base text-gray-700 mobile:text-sm">
+								Suporte técnico de computadores e Equipamentos. Help Desk,
+								fornece suporte técnico e assistência aos usuários da empresa.
+								Diagnóstico de Problemas, atendimento a chamados, gerenciamento
+								de senhas.
+							</p>
+							<span className="mt-1 block text-sm font-semibold text-gray-400 mobile:text-sm">
+								2019 - 2021
+							</span>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,3 +1,4 @@
+import Content from "@/components/Content/Content";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/NavBar/Navbar";
 import Title from "@/components/Title/Title";
@@ -12,33 +13,8 @@ export default function AboutMe() {
 			</Head>
 			<Navbar path="/Contact">Contato</Navbar>
 			<div>
-				<article className="bg-white m-16 p-8 grid gap-6 indent-8 text-xl text-justify mobile:text-sm">
-					<Title>Sobre Mim</Title>
-					<p>
-						Sou desenvolvedor Front-end especializado em criar interfaces
-						elegantes e altamente funcionais. Com três anos de experiência em
-						desenvolvimento e um diploma recente em Engenharia da Computação,
-						estou constantemente aprimorando minhas habilidades de programação.
-						Estou sempre em busca de novos desafios e oportunidades de
-						colaboração para ampliar meus conhecimentos e contribuir com
-						projetos inovadores.
-					</p>
-					<p>
-						Minha experiência inclui HTML5, CSS3, JavaScript e TypeScript,
-						essenciais para entregar designs sofisticados e experiências de
-						usuário intuitivas. Tenho experiência em diversos frameworks CSS,
-						como Tailwind CSS, Bootstrap e Material UI, além de dominar
-						bibliotecas como React.js e o framework Next.js. No back-end, tenho
-						conhecimento de Python, Node.js e ASP.NET, além de SQL, MySQL,
-						MongoDB e Firebase Realtime Database. Estou sempre em busca de novos
-						desafios e oportunidades de colaboração para ampliar meus
-						conhecimentos e contribuir com projetos inovadores.
-					</p>
-				</article>
-				<section className="bg-white m-16 p-8">
-					<Title>Trajetoria</Title>
-					<Trajectory />
-				</section>
+				<Content />
+				<Trajectory />
 			</div>
 			<Footer />
 		</div>
