3e2c995837a087877fe73bb54e4b502549cf5ccf
Rebeca Lameira de Rezende <beca.rezendel@gmail.com>
2024-02-06 19:11:44 -0400

    Fix.

--- components/Content/Content.tsx
+++ components/Content/Content.tsx
@@ -4,7 +4,7 @@ const Content = () => {
 		<article className="bg-white m-16 p-8 grid gap-6 indent-8 text-xl text-justify mobile:text-sm mobile:m-8 dark:bg-black">
 			<Title>Sobre Mim</Title>
 			<p>
-				Sou desenvolvedor Front-end especializado em criar interfaces elegantes
+				Sou desenvolvedora Front-end especializada em criar interfaces elegantes
 				e altamente funcionais. Com três anos de experiência em desenvolvimento
 				e um diploma recente em Engenharia da Computação, estou constantemente
 				aprimorando minhas habilidades de programação. Estou sempre em busca de
